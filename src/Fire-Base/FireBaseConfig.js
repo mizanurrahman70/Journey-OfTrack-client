dac2bdee85e7c6cca86d61762c73e48075c2f909
mizanurrahman70 <mizanurrahmanofficial70.com>
2024-04-26 01:42:15 +0600

    env file add

--- src/Fire-Base/FireBaseConfig.js
+++ src/Fire-Base/FireBaseConfig.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBouQV9bUOhV-kVQ3kfiR5yi6wFRrN5fR4",
-  authDomain: "torist-side.firebaseapp.com",
-  projectId: "torist-side",
-  storageBucket: "torist-side.appspot.com",
-  messagingSenderId: "1006351011528",
-  appId: "1:1006351011528:web:2551d97ab6601084a89ffd"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env. VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.ITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
